2e6429fe9e1252c179a47906f1c33bdcd4aaed97
alexbvw <alex@acreative.co.za>
2025-03-06 12:47:14 +0200

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   webflow_service_url: 'https://localhost:444/v1/webflow/',
   redirect_uri: 'https%3A%2F%2Fwriteflow.acreative.co.za%2Fredirect',
   token: '0b37b08751c08ecdd957867e38e30009098f7c6f53045074ce2a449fc9de072a',
-  client_id: 'b508ea1817364eab97ffd0a0757e9fd25f20a5398385b9752481e6009e3d628d',
+  client_id: '039ee85f25d56b450950fbc2b6b80b3125a6d04c50db9dc2cac0b92ed6cc0e7a',
   scope: 'authorized_user%3Aread%20cms%3Aread%20cms%3Awrite%20forms%3Aread%20sites%3Aread%20users%3Aread%20users%3Awrite%20assets%3Aread%20assets%3Awrite',
 };
 
