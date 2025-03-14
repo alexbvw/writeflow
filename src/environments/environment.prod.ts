ab66822b2e659d913167792d4732c768ff714836
alexbvw <alex@acreative.co.za>
2025-03-14 22:22:48 +0200

    scope

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,6 @@ export const environment = {
   webflow_auth_url: 'https://webflow.com/oauth/authorize',
   webflow_service_url: 'https://writeflowapi.acreative.co.za:444/v1/webflow/',
   redirect_uri: 'https%3A%2F%2Fwriteflow.acreative.co.za%2Fredirect',
-  client_id: 'b508ea1817364eab97ffd0a0757e9fd25f20a5398385b9752481e6009e3d628d',
+  client_id: '039ee85f25d56b450950fbc2b6b80b3125a6d04c50db9dc2cac0b92ed6cc0e7a',
   scope: 'assets%3Aread+assets%3Awrite+authorized_user%3Aread+cms%3Aread+cms%3Awrite+forms%3Aread+sites%3Aread+users%3Aread+users%3Awrite',
-};
-
+}; 
