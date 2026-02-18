71714273f5025cd41a0d9e796fab3c60eeb2eb95
Alex van Wyk <alex@acreative.co.za>
2026-02-18 23:39:31 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   response_type: 'code',
   webflow_base_url: 'https://webflow.com/',
   webflow_auth_url: 'https://webflow.com/oauth/authorize',
-  webflow_service_url: 'https://writeflowapi.acreative.co.za:444/v1/webflow/',
+  webflow_service_url: 'https://writeflow-api.alexbvanwyk.co.za:444/v1/webflow/',
   redirect_uri: 'https%3A%2F%2Fwriteflow.acreative.co.za%2Fredirect',
   client_id: '039ee85f25d56b450950fbc2b6b80b3125a6d04c50db9dc2cac0b92ed6cc0e7a',
   scope: 'assets%3Aread+assets%3Awrite+authorized_user%3Aread+cms%3Aread+cms%3Awrite+forms%3Aread+sites%3Aread+users%3Aread+users%3Awrite',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   response_type: 'code',
   webflow_base_url: 'https://webflow.com/',
   webflow_auth_url: 'https://webflow.com/oauth/authorize',
-  webflow_service_url: 'https://localhost:444/v1/webflow/',
+  webflow_service_url: 'https://writeflow-api.alexbvanwyk.co.za:444/v1/webflow/',
   redirect_uri: 'https%3A%2F%2Fwriteflow.acreative.co.za%2Fredirect',
   token: '0b37b08751c08ecdd957867e38e30009098f7c6f53045074ce2a449fc9de072a',
   client_id: '039ee85f25d56b450950fbc2b6b80b3125a6d04c50db9dc2cac0b92ed6cc0e7a',
